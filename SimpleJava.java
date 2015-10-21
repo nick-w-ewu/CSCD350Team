a7c2225485af1d6f0d3c0be988dd9336b48e2628
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-10-21 10:17:14 -0700

    Changed what program prints

--- SimpleJava.java
+++ SimpleJava.java
@@ -2,11 +2,7 @@ public class SimpleJava
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-		System.out.println("This is the master branch");
-=======
 		System.out.println("This program does nothing");
->>>>>>> nick_assignment5
 	}
 	
 }
